bb896a61c28ffc9ee75c10e8190d35cbc5fb2626
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-10 17:58:36 +0530

    find max string

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Console.WriteLine($"Maximum String is {program.findMaxString("A", "P", "B")}");
+        }
+        public string findMaxString(string str1, string str2, string str3)
+        {
+            if (str1.CompareTo(str2) > 0 && str1.CompareTo(str3) > 0)
+            {
+                return str1;
+            }
+            else if (str2.CompareTo(str3) > 0 && str2.CompareTo(str1) > 0)
+            {
+                return str2;
+            }
+            else
+            {
+                return str3;
+            }
+        }
+    }
+}
