58e07fbd57b62177bf88291718f90f1b9361164d
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-10 18:11:58 +0530

    taking more than 3 parameter

---
+++ FindMaxByGeneric.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class FindMaxByGeneric<T>
+    {
+        public T[] arrayofElement;
+
+        public FindMaxByGeneric(T[] arrayofElement)
+        {
+            this.arrayofElement = arrayofElement;
+        }
+        public T[] Sorting(T[] arrayofElements)
+        {
+            Array.Sort(arrayofElements);
+            return arrayofElements;
+        }
+        public T MaxarrayofElement(T[] arrayofElements)
+        {
+            var sorted_arrayofElements = Sorting(arrayofElements);
+            return sorted_arrayofElements[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxarrayofElement(arrayofElement);
+            return max;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayOfIntegers = { 1, 2, 3, 4, 5, 6 };
+            float[] arrayOfFloats = { 5.2f, 3.6f, 4.7f, 8.7f, 9.8f };
+            string[] arrayOfStrings = { "a", "b", "c", "d", "e", "f" };
+            FindMaxByGeneric<int> findMaxInt = new FindMaxByGeneric<int>(arrayOfIntegers);
+            FindMaxByGeneric<float> findMaxDouble = new FindMaxByGeneric<float>(arrayOfFloats);
+            FindMaxByGeneric<string> findMaxString = new FindMaxByGeneric<string>(arrayOfStrings);
+
+            Console.WriteLine($"Max integer value is {findMaxInt.MaxMethod()}");
+            Console.WriteLine($"Max Float value is {findMaxDouble.MaxMethod()}");
+            Console.WriteLine($"Maximum string is {findMaxString.MaxMethod()}");
+        }
+    }
+}
